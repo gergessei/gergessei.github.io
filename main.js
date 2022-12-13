8ccf0956d169aa625ac310f23341f6d5e39692ec
gergespui <119081422+gergessei@users.noreply.github.com>
2022-12-13 03:26:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+   /* UNA VEZ Q OCURRIO LOAD (SE CARGO LA PAGINA)
+   SETTIMEOUT ESPERA 3 SEGUNDOS PARA EJECUTAR EL CODIGO
+   QUE CAMBIA EL LOADER POR LOADER2 */
+    setTimeout(() => {
+        document.getElementById("loader").classList.toggle("loader2")
+      }, 3190);
+    
+})
+
